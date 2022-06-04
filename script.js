35c7a79d5a280f02dd975f93b5e376572dc81593
Jeanbutar24 <jeanbutar24@gmail.com>
2022-06-05 00:25:53 +0700

    nambah fitur

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
-"strick";
+$(".search-button").on("click", function () {
+  $.ajax({
+    url: "http://www.omdbapi.com/?apikey=21350dc7&s=",
 
-$.ajax({
-  url: "http://www.omdbapi.com/?apikey=21350dc7&s=HERO",
+    success: (data) => {
+      const movies = data.Search;
+      let card = "";
 
-  success: function (data) {
-    const movies = data.Search;
-    let card = "";
-
-    movies.forEach(function (e) {
-      card += `
-                <div class="col-md-3 my-5">
-                <div class="card">
-                <img src="${e.Poster}" class="card-img-top" />
-                <div class="card-body">
-                    <h5 class="card-title">${e.Title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${e.Year}</h6>
-                    <a href="#" class="btn btn-primary modal-detail-button"  data-bs-toggle="modal" data-bs-target="#exampleModal"
-                     data-imdbid =${e.imdbID} >show details</a>
-                </div>
-                </div>
-                </div>
-        `;
-    });
-    $(".data-film").html(card);
-    $(".modal-detail-button").on("click", function () {
-      console.log($(this).data("imdbid"));
-    });
-  },
-  error: function (xhr, exception) {
-    var msg = "";
-    if (xhr.status === 0) {
-      msg = "Not connect.\n Verify Network." + xhr.responseText;
-    } else if (xhr.status == 404) {
-      msg = "Requested page not found. [404]" + xhr.responseText;
-    } else if (xhr.status == 500) {
-      msg = "Internal Server Error [500]." + xhr.responseText;
-    } else if (exception === "parsererror") {
-      msg = "Requested JSON parse failed.";
-    } else if (exception === "timeout") {
-      msg = "Time out error." + xhr.responseText;
-    } else if (exception === "abort") {
-      msg = "Ajax request aborted.";
-    } else {
-      msg = "Error:" + xhr.status + " " + xhr.responseText;
-    }
-  },
+      movies.forEach((e) => {
+        card += showsCard(e);
+      });
+      $(".data-film").html(card);
+      $(".modal-detail-button").on("click", function () {
+        $.ajax({
+          url:
+            "http://www.omdbapi.com/?apikey=21350dc7&i=" +
+            $(this).data("imdbid"),
+          success: (data) => {
+            const movieDetail = showDetail(data);
+            $(".modal-body").html(movieDetail);
+          },
+        });
+      });
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
 });
+
+function showsCard(e) {
+  return `
+  <div class="col-md-3 my-5">
+  <div class="card">
+  <img src="${e.Poster}" class="card-img-top" />
+  <div class="card-body">
+      <h5 class="card-title">${e.Title}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${e.Year}</h6>
+      <a href="#" class="btn btn-primary modal-detail-button"  data-bs-toggle="modal" data-bs-target="#exampleModal"
+       data-imdbid =${e.imdbID} >show details</a>
+  </div>
+  </div>
+  </div>
+`;
+}
+
+function showDetail(data) {
+  return `<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3"><img src ='${data.Poster}' class = "img-fluid"/></div>
+    <div class="col-md">
+      <ul>
+        <li>Title :<strong>${data.Title}</strong> (${data.Year})</li>
+        <li>Actors :<strong>${data.Actors}</strong></li>
+        <li>Director :<strong>${data.Director}</strong></li>
+        <li>Language :<strong>${data.Language}</strong></li>
+        <li>Plot : ${data.Plot}</li>
+      </ul>
+    </div>
+  </div>
+</div>x`;
+}
